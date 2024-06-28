8945833872a96ad1017d44a9735532d0d36e2522
Marcel Hild <hild@b4mad.net>
2024-06-28 16:10:27 +0200

    commentator session type

--- telemetry/pitcrew/application/commentator_application.py
+++ telemetry/pitcrew/application/commentator_application.py
@@ -5,11 +5,12 @@ from telemetry.pitcrew.application.response import ResponseTts
 class CommentatorApplication(Application):
     def __init__(self, session, history, coach):
         super().__init__(session, history, coach)
-        self.car = self.session.car
-        txt = f"You've selected the {self.car} for this session."
-        message = ResponseTts(txt)
-        self.send_response(message)
+        if session.session_type and session.session_type.type == "NewSession":
+            self.car = self.session.car
+            txt = f"{self.car}"
+            message = ResponseTts(txt)
+            self.send_response(message)
 
     def notify(self, distance, telemetry, now):
-        self.log_debug(f"notify: {distance} {telemetry}")
+        # self.log_debug(f"notify: {distance} {telemetry}")
         pass
